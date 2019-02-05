6c2868ad4ef60ff4ee8ca1f8627bb27f5ed76ce8
Anton <antonkorotkoff@gmail.com>
2019-02-05 21:43:47 +0200

    geo ,tests

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect')
 
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -8,6 +8,18 @@ describe('generateMessage', () => {
     var text = 'Text'
     var message = generateMessage(from, text)
     expect(message).toInclude({from, text})
+    expect(typeof message.createdAt).toBe('number')
+  })
+})
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'From'
+    var latitude = 50
+    var longitude = 36
+
+    var message = generateLocationMessage(from, latitude, longitude)
+    expect(message).toInclude({from, url: `https://www.google.com/maps?q=${latitude},${longitude}`})
     expect(typeof message.createdAt).toBe('number')
   })
 })
