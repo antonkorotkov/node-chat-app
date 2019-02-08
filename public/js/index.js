0c386339641c5d2c2111696ad411efdee217eb01
Anton <antonkorotkoff@gmail.com>
2019-02-08 20:44:09 +0200

    css

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault()
 
+  var messageTextbox = jQuery('[name=message]')
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function (data) {
-    console.log('Got it', data)
+    messageTextbox.val('')
   })
 })
 
@@ -41,13 +43,16 @@ var locationButton = jQuery('#send-location')
 locationButton.on('click', function () {
   if (!navigator.geolocation) return alert('Geolocation not supported by your browser.')
 
+  locationButton.attr('disabled', 'disabled').text('Sending Location...')
+
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position)
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
+    locationButton.removeAttr('disabled').text('Send Location')
   }, function () {
     alert('Unable to fetch location.')
+    locationButton.removeAttr('disabled').text('Send Location')
   })
 })
